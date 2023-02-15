f4d4b3d7eb1d764b65204151c7fac2ae27b3ad25
b3rking <ishimweeschylebersy@gmail.com>
2023-02-15 19:27:33 +0200

    feat: add comment component

---
+++ resources/views/livewire/comment.blade.php
@@ -0,0 +1,18 @@
+<div class="">
+    <div class="">
+        <h1>Comments</h1>
+    </div>
+    <div class="">
+        <input type="text">
+        <button>Add</button>
+    </div>
+    <div class="">
+        <div class="">
+            <strong>Some username</strong>
+            <small>3 minutes ago</small>
+        </div>
+        <div class="">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit autem nostrum, assumenda, reiciendis excepturi tempore laudantium iste nesciunt numquam, delectus tenetur nihil porro ipsam deleniti dignissimos doloribus voluptatem praesentium aspernatur.
+        </div>
+    </div>
+</div>
